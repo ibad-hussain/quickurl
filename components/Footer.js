fa2fbf29e629972ab842ed5f4572155431bad077
ibad-hussain <ibadhussain2212@gmail.com>
2025-12-09 11:02:25 +0500

    Fixed styling

--- components/Footer.js
+++ components/Footer.js
@@ -6,8 +6,13 @@ const Footer = () => {
   return (
     <div className={styles.main_footer}>
 
-      <h1 className={styles.footer_heading}>THE FAST LANE OF THE <span className={styles.footer_web}>WEB</span></h1>
-      <div className={styles.footer_subheading}>© 2025 QuickURL. All Rights Reserved.</div>
+      <h1 className={styles.footer_heading}>
+        THE FAST LANE OF THE <span className={styles.footer_web}>WEB</span>
+      </h1>
+
+      <div className={styles.footer_subheading}>
+        © {new Date().getFullYear()} QuickURL. All Rights Reserved.
+      </div>
 
     </div>
   )
